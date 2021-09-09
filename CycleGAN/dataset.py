16c74e6a65a01cd36ce1d8df2ef40ec7730782bc
GeorgeWu <25529932+MagicGeorge@users.noreply.github.com>
2021-09-10 00:14:58 +0800

    add dataset.py

---
+++ CycleGAN/dataset.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/9 23:41
+# @Author  : Wu Weiran
+# @File    : dataset.py
+import os
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+
+class HorseZebraDataset(Dataset):
+    def __init__(self, root_horse, root_zebra, transform=None):
+        self.root_horse = root_horse
+        self.root_zebra = root_zebra
+        self.transform = transform
+
+        self.horse_images = os.listdir(root_horse)
+        self.zebra_images = os.listdir(root_zebra)
+
+        self.horse_len = len(self.horse_images)
+        self.zebra_len = len(self.zebra_images)
+        self.length_dataset = max(self.horse_len, self.zebra_len)
+
+    def __len__(self):
+        return self.length_dataset
+
+    def __getitem__(self, idx):
+        horse_name = self.horse_images[idx % self.horse_len]
+        zebra_name = self.zebra_images[idx % self.zebra_len]
+
+        horse_path = os.path.join(self.root_horse, horse_name)
+        zebra_path = os.path.join(self.root_zebra, zebra_name)
+
+        horse_img = Image.open(horse_path)
+        zebra_img = Image.open(zebra_path)
+
+        if self.transform:
+            horse_img = self.transform(horse_img)
+            zebra_img = self.transform(zebra_img)
+
+        return horse_img, zebra_img
+
+
+if __name__ == '__main__':
+    horse_path = "../data/horse2zebra/train/horse"
+    zebra_path = "../data/horse2zebra/train/zebra"
+    dataset = HorseZebraDataset(horse_path, zebra_path)
+
+    horse, zebra = dataset[0]
+
+    print(horse)
+    print(zebra)

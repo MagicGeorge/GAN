a98d3b21e1744bac9b05a37535a237688006295f
GeorgeWu <25529932+MagicGeorge@users.noreply.github.com>
2021-09-09 15:10:21 +0800

    add discriminator_model.py

---
+++ CycleGAN/discriminator_model.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/9 15:09
+# @Author  : Wu Weiran
+# @File    : discriminator_model.py
--- CycleGAN/generator_model.py
+++ CycleGAN/generator_model.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2021/9/1 19:33
 # @Author  : Wu Weiran
-# @File    : model.py
-a = 1
+# @File    : generator_model.py
